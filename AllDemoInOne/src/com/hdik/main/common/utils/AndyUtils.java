323fb08288d554bd3f3ad60dea2a8ca2226fa858
hardik <hardikbhalodi67@gmail.com>
2013-09-09 20:14:01 +0530

    update util class

--- AllDemoInOne/src/com/hdik/main/common/utils/AndyUtils.java
+++ AllDemoInOne/src/com/hdik/main/common/utils/AndyUtils.java
@@ -330,4 +330,19 @@ public class AndyUtils {
 	public static void showToast(String msg, Context ctx) {
 		Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show();
 	}
+	public static String UppercaseFirstLetters(String str) {
+		boolean prevWasWhiteSp = true;
+		char[] chars = str.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if (Character.isLetter(chars[i])) {
+				if (prevWasWhiteSp) {
+					chars[i] = Character.toUpperCase(chars[i]);
+				}
+				prevWasWhiteSp = false;
+			} else {
+				prevWasWhiteSp = Character.isWhitespace(chars[i]);
+			}
+		}
+		return new String(chars);
+	}
 }
